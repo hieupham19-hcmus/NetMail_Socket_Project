617dba6638e5a9865564daf5928fa949ff8307bb
Pham Ha Hieu <109682107+hieupham19-hcmus@users.noreply.github.com>
2023-11-11 21:25:27 +0700

    Add files via upload

--- Src/Receive_Email.py
+++ Src/Receive_Email.py
@@ -1,120 +1,51 @@
 import socket
 import os
-from base64 import b64encode
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
-from email.header import decode_header
-from email import policy
 from email.parser import BytesParser
+from email import policy
 
-def receive_email(host, pop3_port, userEmail, userPassword):
+def receive_email(host, pop3_port, user_email, user_password):
     try:
-        pop3_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        pop3_client.connect((host, pop3_port))
-        
-        # Receive server greeting
-        response = pop3_client.recv(1024).decode()
-        print(response)
-        
-        # Send USER command
-        pop3_client.sendall(f'USER {userEmail}\r\n'.encode())
-        response = pop3_client.recv(1024).decode()
-        print(response)
+        # Establish connection to the server
+        with socket.create_connection((host, pop3_port)) as pop3_client:
+            print(pop3_client.recv(1024).decode())  # Server greeting
 
-        # Send PASS command
-        pop3_client.sendall(f'PASS {userPassword}\r\n'.encode())
-        response = pop3_client.recv(1024).decode()
-        print(response)
+            # Send USER and PASS commands
+            pop3_client.sendall(f'USER {user_email}\r\n'.encode())
+            print(pop3_client.recv(1024).decode())
 
-        # Send LIST command to get list of emails
-        pop3_client.sendall('LIST\r\n'.encode())
-        response = pop3_client.recv(1024).decode()
-        lines = response.split('\r\n')
-        email_count = sum(1 for line in lines if line and line[0].isdigit())
-        print(response)
-        
-        pop3_client.sendall('UIDL\r\n'.encode())
-        uidl_response = pop3_client.recv(1024).decode()
-        uidl_lines = uidl_response.split('\r\n')
-        
-        for i in range(email_count):
-            uidl = uidl_lines[i+1].split(' ')[1]
-            pop3_client.sendall(f'RETR {i+1}\r\n'.encode())
+            pop3_client.sendall(f'PASS {user_password}\r\n'.encode())
+            print(pop3_client.recv(1024).decode())
+
+            # Retrieve list of emails
+            pop3_client.sendall('LIST\r\n'.encode())
+            response = pop3_client.recv(1024).decode()
+            print(response)
             
-            response = pop3_client.recv(16384).decode()
-            # save to pathfolder
-            current_directory = os.getcwd()
-            # Add filer
-            inbox_path = os.path.join(current_directory, classify_email(response))
-            os.makedirs(inbox_path, exist_ok=True)
-            with open(f'{inbox_path}/{uidl}', 'w') as f:
-                f.write(response)
-
-        # Close the connection
-        pop3_client.sendall('QUIT\r\n'.encode())
-        pop3_client.close()
-    except Exception as e:
-        print(f'Lỗi kết nối POP3: {e}')
-   
-def receive_email(host, pop3_port, userEmail, userPassword):
-    try:
-        pop3_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        pop3_client.connect((host, pop3_port))
-        
-        # Receive server greeting
-        response = pop3_client.recv(1024).decode()
-        print(response)
-        
-        # Send USER command
-        pop3_client.sendall(f'USER {userEmail}\r\n'.encode())
-        response = pop3_client.recv(1024).decode()
-        print(response)
-
-        # Send PASS command
-        pop3_client.sendall(f'PASS {userPassword}\r\n'.encode())
-        response = pop3_client.recv(1024).decode()
-        print(response)
-
-        # Send LIST command to get list of emails
-        pop3_client.sendall('LIST\r\n'.encode())
-        response = pop3_client.recv(1024).decode()
-        lines = response.split('\r\n')
-        email_count = sum(1 for line in lines if line and line[0].isdigit())
-        print(response)
-        
-        pop3_client.sendall('UIDL\r\n'.encode())
-        uidl_response = pop3_client.recv(1024).decode()
-        uidl_lines = uidl_response.split('\r\n')
-        
-        for i in range(email_count):
-            uidl = uidl_lines[i+1].split(' ')[1]
-            pop3_client.sendall(f'RETR {i+1}\r\n'.encode())
+            pop3_client.sendall('UIDL\r\n'.encode())
+            uidl_response = pop3_client.recv(1024).decode()
+            uidl_lines = uidl_response.split('\r\n')
             
-            response = pop3_client.recv(16384).decode()
-            # save to pathfolder
-            current_directory = os.getcwd()
-            # Add filer
-            inbox_path = os.path.join(current_directory, classify_email(response))
-            os.makedirs(inbox_path, exist_ok=True)
-            uidl = uidl.split('.')[0]
-            response = remove_first_last_lines(response)
-            with open(f'{inbox_path}/{uidl}.eml', 'w') as f:
-                f.write(response)
+            # Process each email
+            email_count = sum(1 for line in response.split('\r\n') if line and line[0].isdigit())
+            for i in range(email_count):
+                pop3_client.sendall(f'RETR {i + 1}\r\n'.encode())
+                email_response = pop3_client.recv(16384).decode()
+                
+                # Save email to appropriate directory
+                inbox_path = get_email_folder_address(email_response)
+                os.makedirs(inbox_path, exist_ok=True)
+                uidl = uidl_lines[i+1].split(' ')[1].split('.')[0]
+                
+                with open(os.path.join(inbox_path, f'{uidl}.eml'), 'w') as file:
+                    file.write(remove_metadata(email_response))
 
-        # Close the connection
-        pop3_client.sendall('QUIT\r\n'.encode())
-        pop3_client.close()
+            # Close the connection
+            pop3_client.sendall('QUIT\r\n'.encode())
+
     except Exception as e:
         print(f'Error with POP3 connection: {e}')
 
-def remove_first_last_lines(text):
-    lines = text.splitlines()
-    modified_lines = lines[1:-1]
-    return "\n".join(modified_lines)
-
-def classify_email(email_str):
+def get_email_folder_address(email_str):
     """
     Classify an email into a specific folder based on its subject and content.
     This version accepts email as a string in .msg format.
@@ -153,5 +84,7 @@ def classify_email(email_str):
         return 'Spam'
 
     return 'Inbox'
- 
-  
+
+def remove_metadata(email_str):
+    """Remove metadata from email response."""
+    return '\n'.join(email_str.splitlines()[1:-1])
